79835d22566227a06746fb3e5866a99c31e0c5e8
Dacu02 <you@example.com>
2020-05-22 00:41:01 +0200

    Cooldowns

--- async.py
+++ async.py
@@ -1,25 +1,26 @@
 import brawlstats
 import discord
-import os
+#import os
 from discord.ext import commands
-from discord.ext.commands import Bot,cooldown
-from discord.voice_client import VoiceClient
-from functions import *
-import os
-import requests
-from urllib.request import Request, urlopen
-import aiohttp
-
+#from discord.ext.commands import Bot,cooldown
+#from discord.voice_client import VoiceClient
+#from functions import *
+#import os
+#import requests
+#from urllib.request import Request, urlopen
+#import aiohttp
+TOKEN = 'NzAxMTI1MzExMDQ3NDAxNDc0.XpyBZQ.RAsYlvnkrzI08mwFuXK8QF5K3BM'
+TOKEN2=DaddeToken='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjZjMTU2MzBkLTQ0N2UtNDU3Zi1iNTczLWU4OGI2NjE3Y2NhZSIsImlhdCI6MTU5MDA5NzM0MSwic3ViIjoiZGV2ZWxvcGVyLzAwNWYyOWI0LTVjMTMtYTNkMC1iYzBhLTMwYzQ5NTBkZTVmMCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMzcuMTYwLjY0LjE1NyJdLCJ0eXBlIjoiY2xpZW50In1dfQ.nXcEEkmIDFmG0KAI3FBbQUql-aZ7-izRYF5OXr5hjAbgxbjgd7bePT7UCvY3td3A2jKp4PxaPLxfgdH1ewv2gw'
 #quotaguard ips = 54.72.12.1, 54.72.77.249
 #quotaguard proxy = http://6cy3e5odaiitpe:gxag60u036717xavs35razjk18s2@eu-west-static-03.quotaguard.com:9293
-quotaURL = 'http://6cy3e5odaiitpe:gxag60u036717xavs35razjk18s2@eu-west-static-03.quotaguard.com:9293'
+#quotaURL = 'http://6cy3e5odaiitpe:gxag60u036717xavs35razjk18s2@eu-west-static-03.quotaguard.com:9293'
 bot = commands.Bot(command_prefix='^' , description = "Qlash Bot ")
-#myclient = brawlstats.Client(TOKEN2,is_async=True)
-proxyurl = 'http://6cy3e5odaiitpe:gxag60u036717xavs35razjk18s2@eu-west-static-03.quotaguard.com:9293'
-proxies = {
-"http": os.environ['QUOTAGUARDSTATIC_URL'],
-"https": os.environ['QUOTAGUARDSTATIC_URL']
-}
+myclient = brawlstats.Client(TOKEN2,is_async=True)
+#proxyurl = 'http://6cy3e5odaiitpe:gxag60u036717xavs35razjk18s2@eu-west-static-03.quotaguard.com:9293'
+#proxies = {
+#"http": os.environ['QUOTAGUARDSTATIC_URL'],
+#"https": os.environ['QUOTAGUARDSTATIC_URL']
+#}
 
 #*****************************************************************************************
 
@@ -35,76 +36,85 @@ def checkforrole(member: discord.Member, *roles):
 	return False
 
 #************************************ EVENTS ********************************************
-@bot.event
-async def on_ready():
-    #r = requests.get('http://ip.quotaguard.com', proxies=proxies)
-    print('Logged in as: ',bot.user.name)
-    print('Bot ID: ',bot.user.id)
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://ip.quotaguard.com',proxy=proxyurl) as r:
-            if r.status == 200:
-                js = await r.json()
-                print(js)
-    print('----------------')
-    await bot.change_presence( activity=discord.Activity(type=discord.ActivityType.playing, name=" ^help"))
+# @bot.event
+# async def on_ready():
+#     #r = requests.get('http://ip.quotaguard.com', proxies=proxies)
+#     print('Logged in as: ',bot.user.name)
+#     print('Bot ID: ',bot.user.id)
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get('http://ip.quotaguard.com',proxy=proxyurl) as r:
+#             if r.status == 200:
+#                 js = await r.json()
+#                 print(js)
+#     print('----------------')
+#     await bot.change_presence( activity=discord.Activity(type=discord.ActivityType.playing, name=" ^help"))
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('PermissionError: You do not have the correct role for this command. 😥')
-    if isinstance(error, commands.errors.UserInputError):
-    	await ctx.send('ArguementError: Bad arguement was given. 😕')
-    if isinstance(error, commands.CommandOnCooldown):
-    	await ctx.send('CommandError: Command is on cooldown. 😞')
-    if isinstance(error, commands.CommandNotFound):
-    	await ctx.send('CommandError: Command was not found. 😞')
+# @bot.event
+# async def on_command_error(ctx, error):
+#     if isinstance(error, commands.errors.CheckFailure):
+#         await ctx.send('PermissionError: You do not have the correct role for this command. 😥')
+#     if isinstance(error, commands.errors.UserInputError):
+#     	await ctx.send('ArguementError: Bad arguement was given. 😕')
+#     if isinstance(error, commands.CommandOnCooldown):
+#     	await ctx.send('CommandError: Command is on cooldown. 😞')
+#     if isinstance(error, commands.CommandNotFound):
+#     	await ctx.send('CommandError: Command was not found. 😞')
 
 #***********************************  FUN  ***********************************************
-@commands.cooldown(1, 20, commands.BucketType.user)
+@commands.cooldown(1, 60, commands.BucketType.user)
 @bot.command(name='roll')
 async def roll(ctx):
     await roll_(ctx)
 
 #BucketType.user can be changed to default: global ratelimit, channel: channel ratelimit, guild: server ratelimit, user: user ratelimit /for that command
 #*****************************************************************************************
-@commands.cooldown(1, 20, commands.BucketType.user)
+@commands.cooldown(1, 60, commands.BucketType.channel)
 @bot.command(name='qlash')
 async def qlash(ctx):
     await qlash_(ctx)
 
+#ADMIN
 @bot.command(name='bs-playerinfo',brief='Search for information about a generic ingame player.')
 async def bs_pinfo(ctx,tag):
     await getplayer(ctx,tag)
 
+#ADMIN
 @bot.command(name='bs-claninfo',brief='Search for information about an ingame clan.')
 async def bs_cinfo(ctx,tag):
     await getclan(ctx,tag)
 
+#ADMIN
 @bot.command(name='bs-memberinfo',brief='Search for information about a member within a given clan.')
 async def bs_minfo(ctx,name,ctag):
     await search_member(ctx,name,ctag)
 
+@commands.cooldown(1, 60, commands.BucketType.channel)
 @bot.command(name='qlash-allclans',brief='List all ingame qlash clans.')
 async def qlash_allclans(ctx):
     await qlash_trophies(ctx)
 
+@commands.cooldown(1, 60, commands.BucketType.user)
 @bot.command(name='qlash-clan',brief="Search for information about a specific QLASH clan. Parameter accepts either clan name or clan tag. Clan name has to be identical to the ingame clan name.")
 async def qlash_clan(ctx,name_tag):
     await qlash_cclan(ctx,name_tag)
 
+#ADMIN
 @bot.command(name='set')
 async def set(ctx,gametag):
     await set_(ctx,gametag)
 
+#ADMIN
 @bot.command(name='clan-add',brief='Add a qlash clan to the database. Parameters require <tag> and <clan_name>')
 async def clan_add(ctx,tag,*cname):
     await clan_add_(ctx,tag,*cname)
 
+#ADMIN
 @bot.command(name='clan-remove',brief='Remove a qlash clan from the database. Parameter only requies <clan_name>')
 async def clan_remove(ctx,*cname):
     await clan_remove_(ctx,*cname)
 
+#ADMIN
 @bot.command(name='qlash-clan-members',brief='Shows a list of all members of a given QLASH clan. Parameter accepts either clan name or clan tag. Clan name must be identical to the ingame clan name.')
 async def qlashclanmembers(ctx,clan_or_tag):
     await GetClanMembers(ctx,clan_or_tag)
