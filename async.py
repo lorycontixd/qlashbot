510e0749c5726e6a91f3d3eda3fd8fc73f9a0335
subenix0 <65815606+subenix0@users.noreply.github.com>
2020-05-24 09:24:32 +0200

    aiohttp

--- async.py
+++ async.py
@@ -10,7 +10,6 @@ from descriptions import *
 import requests
 import urllib
 from urllib.request import Request, urlopen
-import aiohttp
 TOKEN = 'NzAxMTI1MzExMDQ3NDAxNDc0.XpyBZQ.RAsYlvnkrzI08mwFuXK8QF5K3BM'
 #quotaguard ips = 54.72.12.1, 54.72.77.249
 #quotaguard proxy = http://6cy3e5odaiitpe:gxag60u036717xavs35razjk18s2@eu-west-static-03.quotaguard.com:9293
