fa7861e29f32d7d1f77ae8835d241dd968fcf8ca
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-05-26 11:31:42 +0200

    fix

--- checks.py
+++ checks.py
@@ -21,18 +21,3 @@ async def Check(ctx,member):
         await ctx.send("You do not have the permission for this command! ")
         return False
     return True
-
-watchouts = ['spongebob']
-async def member_join_check(member:discord.Member):
-    mychannel = bot.get_channel(int(qlash_bot))
-    membername = str(member.name).lower()
-    for item in watchouts:
-        if item in membername:
-            embed=discord.Embed(title="Suspicious member has joined the server"+str(member), color=0xe32400)
-            embed.set_author(name="QLASH Bot")
-            embed.add_field(name="Account Creation Date", value=str(member.created_at), inline=True)
-            embed.add_field(name="User ID", value=str(member.id), inline=True)
-            embed.add_field(name="Mentionable", value=str(member.mention), inline=True)
-            embed.add_field(name="Status", value=str(member.status), inline=True)
-            embed.set_footer(text="Created by Lore")
-            await mychannel.send(embed=embed)
