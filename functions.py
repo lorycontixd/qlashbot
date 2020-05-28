2d165c1106f139e0331c8252fe6414f69ea68398
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-05-28 16:21:54 +0200

    test

--- functions.py
+++ functions.py
@@ -61,18 +61,17 @@ to_zone = tz.tzlocal() #local
 
 
 async def testfunction(ctx):
-    channel = bot.get_channel(int(bot_testing))
+    channel = bot.get_channel(int(ig_role_channel))
     qlashbotch = bot.get_channel(int(qlash_bot))
     messages = await channel.history(limit=4).flatten()
     for message in messages:
         att_list = message.attachments
-        if message.author.name=="Lore":
+        if len(att_list)!=0:
             author = message.author
             await author.create_dm()
             await author.dm_channel.send("Hello "+str(author.name)+". Can you please tell me within 5 minutes if you are from Europe or from America? It is important that you respond quickly to this message with Europe or America.\nCiao "+str(author.name)+". Mi potresti per favore dire (entro 5 minuti) se sei Europeo oppure Americano? è importante che rispondi a questo messaggio con Europa o America")
             msg = await bot.wait_for('message',timeout=350.0)
             await qlashbotch.send("Member: "+str(author)+"\tAnswer: "+str(msg.content))
-            print("Role IG-Tournament given to member: "+str(author))
 
 #*****************************************************************************************************************
 #*****************************************************************************************************************
