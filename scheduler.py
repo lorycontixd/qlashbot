304cc8df31b17f5d91898509542e94a90e6095c6
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-03 16:15:03 +0200

    .

--- scheduler.py
+++ scheduler.py
@@ -15,7 +15,7 @@ async def addsingle(date,member):
     }
     coll_membercount.insert_one(mydict)
 
-async def record():
+def record():
     guild = bot.get_guild(int(qlash_bs_id))
     membercount = guild.member_count
     #membercount = 14540
