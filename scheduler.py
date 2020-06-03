9be990949fb1a48a64af13eafc1b4a493ec29d90
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-03 15:48:50 +0200

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -45,10 +45,12 @@ async def analyze(ctx):
         date = int(date)
         list_date.append(date)
         list_members.append(mcount)
-    plt.title("Member counts for "+month+" "+year)
+    plt.title("Member counts for "+month+" "+year
+    plt.xlabel("Date of "+month)
+    plt.ylabel("Number of members in server")
     plt.plot(list_date,list_members)
     pathname = './graphs/'+month+year+'.png'
-    plt.savefig(pathname)
+    plt.savefig(pathname,bbox_inches='tight')
     #cloudinary.uploader.upload(pathname)
     await ctx.send(file=discord.File(pathname))
 
