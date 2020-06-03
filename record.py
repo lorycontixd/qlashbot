9c13b4a16bb6336d59623fbb633f124ce63db925
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-03 20:40:25 +0200

    .

--- record.py
+++ record.py
@@ -2,9 +2,23 @@
 
 import asyncio
 from scheduler import *
+from instances import *
+
+async def record2():
+    guild = bot.get_guild(int(qlash_bs_id))
+    membercount = guild.member_count
+    #membercount = 14540
+    today = date.today()
+    mydict = {
+        "Date":str(today),
+        "Members":int(membercount)
+    }
+    coll_membercount.insert_one(mydict)
+    #msg = await ctx.send("Registered today's member count")
+
 
 async def daily_record():
-    await record()
+    await record2()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(daily_record())
