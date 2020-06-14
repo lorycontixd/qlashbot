ff722e3b6dda5cce939bb246be961c9e645a171e
subenix0 <65815606+subenix0@users.noreply.github.com>
2020-06-14 11:25:05 +0200

    cleaned up the file

--- scripts/brawlstats-counter/count-participants.py
+++ scripts/brawlstats-counter/count-participants.py
@@ -21,29 +21,6 @@ def _retrieve_playerName(htmlPage):
 def _retrieve_playerClub(htmlPage):
     return htmlPage.xpath('//div[@class="_3lMfMVxY-knKo2dnVHMCWG _21sSMvccqXG6cJU-5FNqzv yVyPKdb4lsiRak5TAnxs3"]//..//div[2]//div/text()')[0].split("\n")[1].strip()
 
-# def _print_clubs(clubs, print_found = True, print_invalid = True, print_not_found = True):
-#     for k in clubs:
-#         if k == INVALID_CLUB and not print_invalid:
-#             continue
-#         elif k == NOT_FOUND_CLUB and not print_not_found:
-#             continue
-#         elif not print_found and k != NOT_FOUND_CLUB and k != INVALID_CLUB:
-#             continue
-#         print(k, len(clubs[k]))
-
-# def _print_members(clubs, print_found = False, print_invalid = True, print_not_found = True):
-#     for k in clubs:
-#         if k == INVALID_CLUB and not print_invalid:
-#             continue
-#         elif k == NOT_FOUND_CLUB and not print_not_found:
-#             continue
-#         elif not print_found and k != NOT_FOUND_CLUB and k != INVALID_CLUB:
-#             continue
-#         print("{CLUB}:".format(CLUB = k))
-#         for members in clubs[k]:
-#             gametag, playerName = members
-#             print(gametag,playerName)
-
 def _print(clubs, print_clubs = True, print_members = False, print_found = True, print_invalid =  False, print_not_found = False):
     for k in clubs:
         if k == INVALID_CLUB and not print_invalid:
@@ -101,28 +78,6 @@ def main_program():
     session = requests.Session()
     session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0'})
     clubs = read_tags(session)
-    # print("Nothing#000")
-    # _print_clubs(clubs, False, False, False)
-    # print("")
-    # print("Not found#001")
-    # _print_clubs(clubs, False, False, True)
-    # print("")
-    # print("Invalid#010")
-    # _print_clubs(clubs, False, True, False)
-    # print("")
-    # print("Members#100")
-    # _print_clubs(clubs, True, False, False)
-    # print("")
-    # print("Members and not found#101")
-    # _print_clubs(clubs, True, False, True)
-    # print("")
-    # print("Members and invalid#110")
-    # _print_clubs(clubs, True, True, False)
-    # print("")
-    # print("All#111")
-    # _print_clubs(clubs, True, True, True)
-    # print("")
-    # _print_members(clubs)
 
     print ("These members were not found:")
     print("")
