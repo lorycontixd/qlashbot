e210312e1a2778c47c0c63a11db4e37e182d4fdd
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-18 16:30:59 +0200

    Update games.py

--- games.py
+++ games.py
@@ -1,10 +1,10 @@
 from instances import *
-async def check_message_reaction(payload):
+
+#***************** treasure_hunt *****************
+async def game1_reaction(payload):
     ch = bot.get_channel(payload.channel_id)
     if ch.id == int(testchannel):
-        print("in test channel")
-        if payload.message_id == 723161253631098931:
-            print("message 3")
+        if payload.message_id == 723182220910526486:
             msg3 = await ch.fetch_message(int(payload.message_id))
             list = msg3.reactions
             if payload.emoji.name=='qlash':
@@ -13,7 +13,25 @@ async def check_message_reaction(payload):
                 def check(message):
                     return message.author == payload.member
 
-                reply = await bot.wait_for('message', check=check)
-                if reply.content == 'qlashforthewin':
-                    await ch.send("Well done "+payload.member.mention+". You won!")
+                reply = await bot.wait_for('message',timeout=30.0, check=check)
+                if reply.content == 'Luca Pagano':
+                    await payload.member.create_dm()
+                    await payload.member.dm_channel.send("Well done +"str(payload.member.name+"for completing step 1.\n \nThe tip for the next step is the following:\n**I think I know the nickname of the brawler posted from Qlash_brawlstars on the 27th May**")
+                    role = discord.utils.get(message.guild.roles, name="step1")
+                    await payload.member.add_roles(role)
                     return
+                else:
+                    await payload.member.create_dm()
+                    await payload.member.dm_channel.send("Sorry wrong password!")
+                    return
+
+async def game1_nickname(before,after):
+    role1 = discord.utils.get(message.guild.roles, name="step1")
+    if role1 not in before.roles and role1 not in after.roles:
+        return
+    if before.nick != after.nick:
+        if str(after.nick).lower() == 'bibi':
+            await after.create_dm()
+            await after.dm_channel.send("You passed step 2, well done!")
+            role2 = discord.utils.get(message.guild.roles, name="step2")
+            await after.add_roles(role2)
