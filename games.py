4c77cb1e64e21195cc5b4a2b9f41fc164900d6b4
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-18 15:26:16 +0200

    .

--- games.py
+++ games.py
@@ -2,9 +2,10 @@
 async def check_message_reaction(reaction,user):
     msg = reaction.message
     if msg.id == 723161253631098931:
+        print("reaction added to 3"+str(reaction.emoji))
         list = msg.reactions
         for i in range(len(list-1)):
-            if list[i].emoji == ':qlash:':
+            if list[i].emoji.name == ':qlash:':
                 if list[i+1].emoji == '🔝':
                     users = await list[i].users().flatten()
                     users2 = await list[i+1].users().flatten()
