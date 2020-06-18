5740e450e34ccaf5bbed17c818aaa15ed3212da7
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-18 16:48:21 +0200

    Update games.py

--- games.py
+++ games.py
@@ -8,16 +8,16 @@ async def game1_reaction(payload):
             msg3 = await ch.fetch_message(int(payload.message_id))
             list = msg3.reactions
             if payload.emoji.name=='qlash':
-                await ch.send("Hi "+payload.member.mention+". Enter the password within 30 seconds please")
+                await ch.send("Hi "+payload.member.mention+". In order to access the next checkpoint, please answer this quiz:\nWhen was Qlash founded?")
 
                 def check(message):
                     return message.author == payload.member
 
                 reply = await bot.wait_for('message',timeout=30.0, check=check)
-                if reply.content == 'Luca Pagano':
+                if reply.content == '2017':
                     await payload.member.create_dm()
-                    await payload.member.dm_channel.send("Well done "+str(payload.member.name)+"for completing step 1.\n \nThe tip for the next step is the following:\n**I think I know the nickname of the brawler posted from Qlash_brawlstars on the 27th May**")
-                    role = discord.utils.get(message.guild.roles, name="step1")
+                    await payload.member.dm_channel.send("Well done "+str(payload.member.name)+" for completing step 1.\n \nThe tip for the next step is the following:\n**I think I know the nickname of the brawler posted from Qlash_brawlstars on the 27th May**")
+                    role = discord.utils.get(after.guild.roles, name="step1")
                     await payload.member.add_roles(role)
                     return
                 else:
