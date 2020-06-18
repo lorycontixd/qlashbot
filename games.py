163491a248904bb9d442980df6dbb28da22ce1db
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-18 15:38:43 +0200

    .

--- games.py
+++ games.py
@@ -5,7 +5,7 @@ async def check_message_reaction(payload):
         print("in test channel")
         if payload.message_id == 723161253631098931:
             print("message 3")
-            msg3 = ch.fetch_message(int(payload.message_id))
+            msg3 = await ch.fetch_message(int(payload.message_id))
             list = msg3.reactions
             if payload.emoji.name=='qlash':
                 print("winner"+str(payload.member))
