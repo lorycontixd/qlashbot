12add421a5063d18903e05f193a4147a8947d03f
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-20 20:06:07 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -12,7 +12,7 @@ class MyCog(commands.Cog):
 
     @tasks.loop(seconds=1.0)
     async def counter(self):
-        await ch.send(self.index)
+        await self.ch.send(self.index)
         self.index += 1
         if self.index == 15:
             cog_unload(self)
