83163f9b8294aa7a647597bafe1cd24da3b3b332
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-20 22:15:05 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -4,26 +4,6 @@ from discord.ext import tasks, commands
 from instances import *
 from datetime import datetime
 
-class MyCog(commands.Cog):
-    def __init__(self):
-        self.index = 0
-        self.sent = False
-        self.counter.start()
-        self.it = bot.get_channel(int(it_general))
-
-    def cog_unload(self):
-        self.counter.cancel()
-
-    @tasks.loop(minutes=1.0)
-    async def counter(self):
-        t = datetime.now()
-        if t.strftime("%h")==22:
-            if self.sent==False:
-                await it.send("Buonanotte!")
-                self.sent=True
-        else:
-            self.sent=False
-
 #ugly workaroudn to trigger immediately
 @apscheduler.scheduled_job('date')
 async def reddit_webhook_now():
@@ -32,7 +12,8 @@ async def reddit_webhook_now():
 #only triggers after 15 minutes (will be fixed in 4.0)
 @apscheduler.scheduled_job('interval', minutes=15)
 async def reddit_webhook():
+    ch = bot.get_channel(int(bot_developer_channel))
     async with aiohttp.ClientSession() as session:
         async with session.get('https://www.reddit.com/r/Brawlstars.json') as resp:
             if resp.status == 200:
-                    print(await resp.text())
+                    await ch.send(await resp.text())
