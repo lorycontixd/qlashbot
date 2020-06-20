69d674eb6cb71af8bfa67c3834be2f8364f2c73a
subenix0 <65815606+subenix0@users.noreply.github.com>
2020-06-20 14:01:45 +0200

    Further proof reading

--- modules/welcome_message.py
+++ modules/welcome_message.py
@@ -23,30 +23,29 @@ WELCOME_MESSAGE_SECOND_SECTION.add_field(name="Twitter", value=constants.QLASH_B
 
 RULES_SECTION = """**Rules**
 
-Before you start to chat in our community channels please read these rules. We inform all our users that by joining this server you give the tacit approval to all the following rules.
+Before you start to chat in our community channels please read these rules. We inform all our users that by joining this server you give the tacit approval to all of the following rules.
 
-This server has members of all nationalities, beliefs, religions, ages and orientations, so be respectful of others while you are here.
+This server has members of all nationalities, beliefs, religions, ages and orientations, so please be respectful of others while you are staying on the server.
 
 **General Rules**
 
 1- Intolerance, racism or any kind of vulgarity is prohibited on this server in any form (audio, text, private chat, tags, images etc.) please to be respectful to every other user, otherwise you will be kicked/banned regardless of your role.
 2- Do not spam. This includes our discord channels and any users on the server; including, but not limited to admins, streamer, players, casters, influencers, moderators and coaches. This also includes spam of symbols, letters, images, links, emotes or videos.
 3- Inappropriate Usernames or Profile Pics are not allowed.
-4- No porn material is permitted in any form on this server, also sexism is forbidden.
+4- No porn or NSFW material is permitted in any form on this server, also sexism is forbidden.
 5- Use common sense. This means no shitposting will be tolerated in any public channel under any means.
-6- No flame will be tolerated in any way, both text and audio.
-7- You can't sell your account here.
-8- BrawlStars Account sharing is forbidden. So don't ask for account pushing or other things related to this topic.
-9- You can't share links to other servers/groups
-(Discord, telegram, etc...) without approval.
-10- You can't promote external Clans without approval.
+6- No flame will be tolerated in any way, both in text and audio.
+7- You can't sell your account here and you have to follow the Terms of Service and other applicable policies by Supercell.
+8- BrawlStars Account sharing is also forbidden. So please don't ask for sharing account for pushing trophies or other things related to this topic.
+9- You can't advertise or share links to other servers/groups (Discord, telegram, etc...) without prior approval of management.
+10- You can't promote unofficial or external clans without prior approval from management.
 
-**Admin/Mod rules**
+**Admin/Moderator rules**
 
-1- Admin/Moderators keep the server under control, if they ask you to stop doing something listen to them.
-2- Don't impersonate other users, especially staff. If they ask you to stop, listen to them.
-3- If you have any question or problem with other user please contact our staff.
-4- Admin/Moderators cannot abuse their role, If they do so report it to another Admin.
+1- Admins and Moderators keep the server under control, if they ask you to stop doing something, please listen to them.
+2- Don't impersonate other users, especially staff. If they ask you to stop, please listen to them.
+3- If you have any question or problem with another user, please contact our staff members and let them know of your experience.
+4- Admins and Moderators are not allowed to abuse their authority. If you feel an Admin or a Moderator does, please report your experience to another Admin.
 """
 
 RULES_SECTION_IMAGE_URL = "https://cdn.discordapp.com/attachments/720193411113680913/723857824874233887/rules_discord.png"
