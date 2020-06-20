e632b656af620d91b09c31ecbd42d0f0822912f9
subenix0 <65815606+subenix0@users.noreply.github.com>
2020-06-20 23:18:04 +0200

    good morning

--- tasks.py
+++ tasks.py
@@ -14,18 +14,16 @@ async def reddit_webhook_now():
 #only triggers after 15 minutes (will be fixed in 4.0)
 @apscheduler.scheduled_job('interval', minutes=15)
 async def reddit_webhook():
-    await bot.wait_until_ready()
     ch = bot.get_channel(int(bot_developer_channel))
     async with aiohttp.ClientSession() as session:
         async with session.get('https://www.reddit.com/r/Brawlstars.json') as resp:
             if resp.status == 200:
-                #print("Sending to channel: "+ch.name)
-                await ch.send(await resp.text())
+                await ch.send("Reddit?")
+                #await ch.send(await resp.text())
 
-@apscheduler.scheduled_job('cron', hour=23, minute=16)
+@apscheduler.scheduled_job('cron', hour=23, minute=19)
 async def goodmorning():
     frasi=[' fagiolini',' stelline',' cuccioli',' patatoni',' bomberoni','!']
-    #await bot.wait_until_ready()
     ch=bot.get_channel(int(it_general))
     temp = random.choice(frasi)
     await ch.send("Buongiorno"+temp)
