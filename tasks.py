e38a9975009e6a80e8d46b2bbc2223903a67ac41
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-20 19:59:14 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,22 +1,17 @@
 from discord.ext import tasks, commands
 
-class MyCog(commands.Cog):
+class MyCog(commands.Cog,ctx):
     def __init__(self):
         self.index = 0
         self.counter.start()
 
     def cog_unload(self):
-        print("stopping...")
+        await
         self.counter.cancel()
 
-    @tasks.loop(seconds=5.0)
-    async def counter(self):
-        print(self.index)
+    @tasks.loop(seconds=1.0)
+    async def counter(self,ctx):
+        await ctx.send(self.index)
         self.index += 1
         if self.index == 15:
             cog_unload(self)
-
-    @counter.before_loop
-    async def before_counter(self):
-        print('waiting...')
-        await self.bot.wait_until_ready()
