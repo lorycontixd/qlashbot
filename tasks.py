6a459b1c67a0ab11080bbb53260d81c3913452bd
subenix0 <65815606+subenix0@users.noreply.github.com>
2020-06-20 22:18:33 +0200

    decreased time and wrote test

--- tasks.py
+++ tasks.py
@@ -10,9 +10,10 @@ async def reddit_webhook_now():
    await reddit_webhook()
 
 #only triggers after 15 minutes (will be fixed in 4.0)
-@apscheduler.scheduled_job('interval', minutes=15)
+@apscheduler.scheduled_job('interval', seconds=10)
 async def reddit_webhook():
     ch = bot.get_channel(int(bot_developer_channel))
+    await ch.send("helo")
     async with aiohttp.ClientSession() as session:
         async with session.get('https://www.reddit.com/r/Brawlstars.json') as resp:
             if resp.status == 200:
