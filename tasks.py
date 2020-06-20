95b0c5bf19e6497558092c4355f422d79a7ee774
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-20 20:03:57 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,17 +1,18 @@
 from discord.ext import tasks, commands
+from instances import *
 
-class MyCog(commands.Cog,ctx):
+class MyCog(commands.Cog):
     def __init__(self):
         self.index = 0
         self.counter.start()
+        self.ch = bot.get_channel(int(bot_developer_channel))
 
     def cog_unload(self):
-        await
         self.counter.cancel()
 
     @tasks.loop(seconds=1.0)
-    async def counter(self,ctx):
-        await ctx.send(self.index)
+    async def counter(self):
+        await ch.send(self.index)
         self.index += 1
         if self.index == 15:
             cog_unload(self)
