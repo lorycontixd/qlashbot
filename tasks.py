a722e24359af50afcdbe1e9d7e2f3f56f8140e3c
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-21 00:54:45 +0200

    Commented reddit function

--- tasks.py
+++ tasks.py
@@ -9,18 +9,18 @@ from datetime import datetime
 
 
 #ugly workaroudn to trigger immediately
-@apscheduler.scheduled_job('date')
-async def reddit_webhook_now():
-   await reddit_webhook()
+#@apscheduler.scheduled_job('date')
+#async def reddit_webhook_now():
+#   await reddit_webhook()
 
 #only triggers after 15 minutes (will be fixed in 4.0)
-@apscheduler.scheduled_job('interval', minutes=15)
-async def reddit_webhook():
-    ch = bot.get_channel(int(bot_developer_channel))
-    async with aiohttp.ClientSession() as session:
-        async with session.get('https://www.reddit.com/r/Brawlstars.json') as resp:
-            if resp.status == 200:
-                await ch.send("Reddit?")
+#@apscheduler.scheduled_job('interval', minutes=15)
+#async def reddit_webhook():
+#    ch = bot.get_channel(int(bot_developer_channel))
+#    async with aiohttp.ClientSession() as session:
+#        async with session.get('https://www.reddit.com/r/Brawlstars.json') as resp:
+#            if resp.status == 200:
+#                await ch.send("Reddit?")
                 #await ch.send(await resp.text())
 
 async def reg_member():
