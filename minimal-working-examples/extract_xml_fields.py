9178867744ccd8c792b902233e89d68ac3ea9359
subenix0 <65815606+subenix0@users.noreply.github.com>
2020-06-21 12:04:03 +0200

    extract fields (mwe)

---
+++ minimal-working-examples/extract_xml_fields.py
@@ -0,0 +1,17 @@
+from xml.etree import ElementTree as ET
+from io import StringIO
+
+def extract_fields(message):
+    string = StringIO()
+    string.write("<banned>")
+    string.write(message)
+    string.write("</banned>")
+    tree = ET.fromstring(string.getvalue())
+
+    fields_dict = tree.find('.//player')
+    string.close()
+    return fields_dict.attrib
+
+fields = extract_fields("""<player name="QLS | Lore" tag="#20VYYG2L" ban="40d" reason="Insults" discord="Lore#5934" />""")
+#{'name': 'QLS | Lore', 'tag': '#20VYYG2L', 'ban': '40d', 'reason': 'Insults', 'discord': 'Lore#5934'}
+print(fields)
