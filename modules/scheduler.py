a9484b78c6f1c40df8f97fb317ae6514fe077dc3
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-21 18:29:11 +0200

    1831

--- modules/scheduler.py
+++ modules/scheduler.py
@@ -12,7 +12,7 @@ def init_scheduler(mongodb):
     }
     executors = {
         'default': ThreadPoolExecutor(20),
-        'processpool': ProcessPoolExecutor(5)
+        'processpool': ProcessPoolExecutor(5) 
     }
     job_defaults = {
         'coalesce': False,
