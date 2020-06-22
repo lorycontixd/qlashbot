1d14dbe3f4d50ed5fa15c10f757b2d58e0380824
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-22 15:27:33 +0200

    Added Get Jobs

--- modules/scheduler.py
+++ modules/scheduler.py
@@ -3,7 +3,6 @@ from pytz import timezone
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.mongodb import MongoDBJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
-
 from tasks import *
 
 def init_scheduler(mongodb):
@@ -19,7 +18,7 @@ def init_scheduler(mongodb):
         'max_instances': 3,
         'misfire_grace_time': 15*60,
     }
-    scheduler = AsyncIOScheduler(job_defaults=job_defaults,timezone=timezone('Europe/Berlin'))
+    scheduler = AsyncIOScheduler(job_defaults=job_defaults,timezone=timezone('Europe/Rome'))
     #scheduler.configure(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
     scheduler.start()
     return scheduler
@@ -34,6 +33,10 @@ def add_default_tasks(scheduler):
     #scheduler.add_job(reg_member, id='reg_member', trigger='cron', hours=22)
     CEST = timezone('Europe/Rome')
     scheduler.add_job(check_banlist_channel, id='check_banlist_channel', trigger='cron',hour='22', minute='0')
-    scheduler.add_job(hello_en,id="hello-en",trigger='cron',hour='15',minute='19')
+    scheduler.add_job(hello_en,id="hello-en",trigger='cron',hour='9',minute='0')
     scheduler.add_job(reg_member,id='register-member',trigger='cron',hour='22',minute='0')
     pass
+
+def _get_jobs(scheduler):
+    list = scheduler.get_jobs()
+    return list
