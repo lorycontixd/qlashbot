929b95b11e42a4e6e62be3fd00212a3b68118a92
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-06-22 22:25:22 +0200

    saints for elblasfemo

--- modules/scheduler.py
+++ modules/scheduler.py
@@ -33,7 +33,7 @@ def add_default_tasks(scheduler):
     #scheduler.add_job(reg_member, id='reg_member', trigger='cron', hours=22)
     CEST = timezone('Europe/Rome')
     scheduler.add_job(check_banlist_channel, id='check_banlist_channel', trigger='cron',hour='22', minute='0')
-    scheduler.add_job(hello_en,id="hello-en",trigger='cron',hour='9',minute='0')
+    #scheduler.add_job(hello_en,id="hello-en",trigger='cron',hour='9',minute='0')
     scheduler.add_job(reg_member,id='register-member',trigger='cron',hour='22',minute='0')
     pass
 
