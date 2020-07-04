f66096df9cded2cc2cfac2f0cca66587bbff4a2f
subenix0 <65815606+subenix0@users.noreply.github.com>
2020-07-04 12:33:44 +0200

    new modules

--- iqloudy/modules/cogs.py
+++ iqloudy/modules/cogs.py
@@ -1,10 +1,14 @@
+from modules.cogs_brawlstars import BrawlStars
 from modules.cogs_moderation import Moderation
 from modules.cogs_weather import Weather
 from modules.cogs_fun import Fun
 from modules.cogs_system import System
+from modules.cogs_permissions import Permissions
 
 async def init_cogs(bot):
     bot.add_cog(Weather())
     bot.add_cog(Fun())
     bot.add_cog(Moderation())
     bot.add_cog(System())
+    bot.add_cog(BrawlStars())
+    bot.add_cog(Permissions())
---
+++ iqloudy/modules/cogs_permissions.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot,cooldown
+from discord.voice_client import VoiceClient
+from instances import *
+from descriptions import *
+from functions import *
+
+class Permissions(commands.Cog,name="Permissions"):
+    def __init__(self):
+        pass
+
+    @commands.has_any_role('DiscordDeveloper')
+    @commands.command(name='check-channel',brief='check channel perms')
+    async def check_channel(self,ctx,channel:discord.TextChannel):
+        await ctx.send("Not yet implemented.")
+
+    @commands.has_any_role('DiscordDeveloper')
+    @commands.command(name='lock-channel',brief='lock channel')
+    async def lock_channel(self,ctx,channel:discord.TextChannel):
+        await ctx.send("Not yet implemented.")
+
+    @commands.has_any_role('DiscordDeveloper')
+    @commands.command(name='make-private-channel',brief='make a private channel')
+    async def make_private_channel(self,ctx,channel:discord.TextChannel):
+        await ctx.send("Not yet implemented.")
+
+    @commands.has_any_role('DiscordDeveloper')
+    @commands.command(name='make-public-channel',brief='make a public channel')
+    async def make_public_Channel(self,ctx,channel:discord.TextChannel):
+        await ctx.send("Not yet implemented.")
