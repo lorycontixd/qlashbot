2256441f56ccfdaebf4e363d77bc71e3e9bf31f6
subenix0 <65815606+subenix0@users.noreply.github.com>
2020-07-04 15:20:45 +0200

    remove unused

--- iqloudy/unused/helptest.py
+++
@@ -1,7 +0,0 @@
-#First implementation for a better Help Command
-import discord
-from discord.ext import commands
-
-_help = commands.DefaultHelpCommand()
-help.no_category='Main Category'
-print(_help.no_category)
