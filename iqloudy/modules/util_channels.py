11e0d355aca7f042be40082984b401f8b788e12e
subenix0 <65815606+subenix0@users.noreply.github.com>
2020-07-05 11:20:26 +0200

    Update util_channels.py

--- iqloudy/modules/util_channels.py
+++ iqloudy/modules/util_channels.py
@@ -4,5 +4,5 @@ role_everyone = 415221296247341066
 async def util_check_channel (cog, ctx, channel):
     for role in channel.overwrites:
         print(role)
-        print(channel.overwrite[role])
+        print(channel.overwrites[role])
     await ctx.send("not yet implemented")
