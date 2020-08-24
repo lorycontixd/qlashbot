146888775f6b6252fed400260a6584614e3fedd6
lorycontixd <56968043+lorycontixd@users.noreply.github.com>
2020-08-25 01:11:17 +0200

    bot_instances module name fix

--- iqloudy/modules/voice_system/library.py
+++ iqloudy/modules/voice_system/library.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.voice_client import VoiceClient
-import instances
+import bot_instances
 import mongodb as mongo
 
 class VoiceSystem(commands.Cog,name="VoiceSystem"):
@@ -9,11 +9,9 @@ class VoiceSystem(commands.Cog,name="VoiceSystem"):
     Class to deal with voice connections.
     """
     def __init__(self):
-        self.waiting = instances.bot.fetch_channel(747588681627336786)
-        self.ch1 = instances.bot.fetch_channel(747588721544790117)
+        self.waiting = bot_instances.bot.fetch_channel(747588681627336786)
+        self.ch1 = bot_instances.bot.fetch_channel(747588721544790117)
     
     @commands.command(name="test",brief="Test command for Voice System")
     def print_channel1(self):
         print(self.ch1)
-
-
